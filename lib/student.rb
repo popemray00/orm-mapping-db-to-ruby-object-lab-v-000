b40c9b0082eacd5ea69ff8fd0be1389b9a6d5c6f
Michael Pope <popemray1986@gmail.com>
2018-03-03 21:07:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,8 @@ class Student
       FROM student
     SQL
 
-    DB[:conn].execute(sql).map { |row| self.new_from_db(row)}
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
     end.first
   end
 
