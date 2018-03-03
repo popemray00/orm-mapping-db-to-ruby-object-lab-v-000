e97bc96ac7fec340b2738e0bc29ca0ac46821ba7
Michael Pope <popemray1986@gmail.com>
2018-03-03 21:13:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   def self.all
     sql = <<-SQL
       SELECT *
-      FROM student
+      FROM students
     SQL
 
     DB[:conn].execute(sql).map do |row|
@@ -23,7 +23,7 @@ class Student
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
-      FROM student
+      FROM students
       WHERE name = ?
       LIMIT 1
     SQL
